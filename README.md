0faf98a4d8b9f952dba9e4ec248d2b54bc84b45a
Frank Rowe <fsrowe@gmail.com>
2014-03-01 15:11:52 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-simplify-geojson-ui
-===================
+mapboilerplate
+==============
+
+Clone to start a new Leaflet map.
+
+Includes Leaflet.js, Bootstrap, and jQuery.
+
+* Leaflet v0.7
+* Bootstrap 3.0.2
+* jQuery v1.10.2
---
+++ js/main.js
@@ -0,0 +1,11 @@
+var simplify = require('simplify-geojson')
+
+$(document).ready(function(){
+
+  var map = new L.Map('map').setView(new L.LatLng(38.85, -77), 7);
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
+
+})
